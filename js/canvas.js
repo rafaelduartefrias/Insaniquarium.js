cb66c623adc8cc0eab30f1e8c70bfe0374c44374
rafaelduartefrias <158203567+rafaelduartefrias@users.noreply.github.com>
2025-10-30 17:02:34 -0300

    Remove game function and call

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,4 @@
 const canvas = document.getElementById('GameCanvas');
 const ctx = canvas.getContext('2d');
-
-function game() {
-  ctx.fillStyle = '#FFFFFF';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-game();
+ctx.fillStyle = '#FFFFFF';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
