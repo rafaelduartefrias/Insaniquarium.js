12f2aa1eea18a3295ec0194badec16091d8c8768
rafaelduartefrias <158203567+rafaelduartefrias@users.noreply.github.com>
2025-10-30 17:06:19 -0300

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,6 @@
-const canvas = document.getElementById('GameCanvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#FFFFFF';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('GameCanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
