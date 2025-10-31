eb73c9fc50094b3db1360d30763b67e2294cf8cd
rafaelduartefrias <158203567+rafaelduartefrias@users.noreply.github.com>
2025-10-31 15:09:42 -0300

    Set canvas background to aquarium image
    Replaced white background with aquarium image.

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('GameCanvas');
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const bg = new Image();
+    bg.src = './img/aquarium1.jpg';
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 });
