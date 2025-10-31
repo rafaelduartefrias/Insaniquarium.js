cc3bf30f6299927b17d170a8bd62610224afec7d
rafaelduartefrias <158203567+rafaelduartefrias@users.noreply.github.com>
2025-10-31 15:22:36 -0300

    Add menu bar drawing functionality to canvas

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('GameCanvas');
     const ctx = canvas.getContext('2d');
     const bg = new Image();
+    const MenuBarIMG = new Image();
     bg.src = './img/aquarium1.jpg';
+    MenuBarIMG.src = './img/menubar.gif'
+    function DrawMenuBar() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const menubarWidth = 640;
+        const menubarHeight = 75;
+        const x = (canvas.width - menubarWidth) / 2;
+        const y = 20;
+        if (MenuBarIMG.complete) {
+           ctx.drawImage(MenuBarIMG, x, y, hudWidth, hudHeight);
+        }
+    }
+    MenuBarIMG.onload = drawMenuBar;
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 });
